d8978f33baf4e0f4903b53366a0b9ac1e1ecaf12
DonatasNoreika <donoras@gmail.com>
2025-10-24 09:00:30 +0300

    Aprašytas Author modelis

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(verbose_name="Vardas", max_length=30)
+    last_name = models.CharField(verbose_name="Pavardė", max_length=30)
+
+    def __str__(self):
+        return f"Autorius {self.first_name} {self.last_name}"
