2eec2b832e2869898acd0c1e7ff8d798b5f89271
DonatasNoreika <donoras@gmail.com>
2025-10-24 09:34:16 +0300

    Aprašytas Book modelis

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -13,3 +13,13 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+class Book(models.Model):
+    title = models.CharField(verbose_name="Pavadinimas")
+    summary = models.TextField(verbose_name="Aprašymas")
+    isbn = models.CharField(verbose_name="ISBN")
+    author = models.ForeignKey(to="Author", verbose_name="Autorius", on_delete=models.SET_NULL, null=True, blank=True)
+    genre = models.ManyToManyField(to="Genre", verbose_name="Žanras")
+
+    def __str__(self):
+        return self.title
