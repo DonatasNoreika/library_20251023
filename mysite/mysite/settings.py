cb19b899c2666a62f2e01a1963ba7749dad3b3d3
DonatasNoreika <donoras@gmail.com>
2025-11-03 09:46:47 +0200

    7: nustatytas prisijungimas/atsijungimas

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,3 +121,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'library/media')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
