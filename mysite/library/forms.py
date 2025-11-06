560f14ffd0342740dabe31cc8a18d53274506b92
DonatasNoreika <donoras@gmail.com>
2025-11-06 08:59:53 +0200

    10: +Vartotojas gali paredaguoti savo duomenis profilio puslapyje

--- mysite/library/forms.py
+++ mysite/library/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import BookReview
+from django.contrib.auth.models import User
 
 class BookReviewForm(forms.ModelForm):
     class Meta:
         model = BookReview
         fields = ['content']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
--- mysite/library/views.py
+++ mysite/library/views.py
@@ -7,7 +7,8 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from .forms import BookReviewForm
+from .forms import BookReviewForm, UserUpdateForm
+from django.contrib.auth.models import User
 
 def index(request):
     num_visits = request.session.get('num_visits', 1)
@@ -105,3 +106,13 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     template_name = "signup.html"
     success_url = reverse_lazy('login')
+
+
+class ProfileUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = User
+    form_class = UserUpdateForm
+    template_name = "profile.html"
+    success_url = reverse_lazy('profile')
+
+    def get_object(self, queryset=...):
+        return self.request.user
