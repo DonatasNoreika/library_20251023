084916176ff3fe655ce7ae77632cf17e01b55e67
DonatasNoreika <donoras@gmail.com>
2025-11-10 10:23:28 +0200

    11: +Prisijungęs darbuotojas (staff) gali ištrinti egzempliorių (DeleteView)

--- mysite/library/urls.py
+++ mysite/library/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path("instances/<int:pk>/", views.BookInstanceDetailView.as_view(), name="instance"),
     path("instances/create/", views.BookInstanceCreateView.as_view(), name="instances_create"),
     path("instances/<int:pk>/update/", views.BookInstanceUpdateView.as_view(), name="instances_update"),
+    path("instances/<int:pk>/delete/", views.BookInstanceDeleteView.as_view(), name="instances_delete"),
 ]
